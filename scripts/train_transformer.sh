6bdf9cc350a631d97e0be08038a5ebd4bb2e0bb8
Maksim Koltiugin <mkoltugin@gmail.com>
2024-07-10 17:25:22 +0300

    push tokenizer into repo

--- scripts/train_transformer.sh
+++ scripts/train_transformer.sh
@@ -1,4 +1,4 @@
 python3 scripts/train_transformer.py \
---dataset_base_dir="data-uncased-15k-10k" \
---ckpts_dir="weights/transformer_uncased/ckpts" \
---logs_dir="weights/transformer_uncased/logs" 
+--dataset_base_dir="data/uncased-15k-10k" \
+--ckpts_dir="checkpoints/transformer_uncased/ckpts" \
+--logs_dir="checkpoints/transformer_uncased/logs" 
