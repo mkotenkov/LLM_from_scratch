739bb1a142b8a1d13480f7fb67010b7ab36b74e1
Maksim Koltiugin <mkoltugin@gmail.com>
2024-07-11 23:55:46 +0300

    rm

--- scripts/_preprocess_data.sh
+++
@@ -1,5 +0,0 @@
-python3 scripts/preprocess_data.py \
---dir_to_save="data/uncased-15k-10k" \
---tokenizer_path="checkpoints/tokenizer/tokenizer_15k_10k_uncased.pkl" \
---articles_for_test=100 \
---articles_for_train=100000
--- scripts/_train_transformer.sh
+++
@@ -1,7 +0,0 @@
-RUN_NAME="transformer_uncased_2000_steps"
-
-python3 /Users/maksimkoltugin/Dev/huawei_LLM_test_task/scripts/train_transformer.py \
---dataset_base_dir="/Users/maksimkoltugin/Dev/huawei_LLM_test_task/data/uncased-15k-10k" \
---ckpts_dir="/Users/maksimkoltugin/Dev/huawei_LLM_test_task/checkpoints/${RUN_NAME}/ckpts" \
---logs_dir="/Users/maksimkoltugin/Dev/huawei_LLM_test_task/checkpoints/${RUN_NAME}/logs" \
---tokenizer_path="/Users/maksimkoltugin/Dev/huawei_LLM_test_task/checkpoints/tokenizer/tokenizer_15k_10k_uncased.pkl"
